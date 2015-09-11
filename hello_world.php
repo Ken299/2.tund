d5001fb913931389b8a38834bdd3eab28fd60f93
Ken Aonurm <kenaon@greeny.cs.tlu.ee>
2015-09-11 11:36:36 +0300

    logic added

--- hello_world.php
+++ hello_world.php
@@ -4,3 +4,33 @@
 	echo $first_name." ".$last_name
 	
 ?>
+<br>
+<?php
+
+	$age = 19;
+	
+	//kui on väiksem kui 18, siis alakas
+	//kui on suurem siis täisealine
+	if($age < 18) {
+		//tõene
+		echo "alakas";
+		
+	} else {
+		//väär
+		echo "täisealine";
+	}
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+8
